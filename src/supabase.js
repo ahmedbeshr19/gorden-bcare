5cdc410eb0f5a3bf10652ee53794f0c99f1fc231
ahmedbeshr19 <ahmadaymanppp@gmail.com>
2026-06-11 14:23:10 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://erooogrlzhuswzfskyce.supabase.co'
-const supabaseAnonKey = 'sb_publishable_uqicBznOu4HKS1KpW5Gxtg_dx_Sp0j8'
+const supabaseUrl = 'https://ynvukudbobtamgklgwdx.supabase.co'
+const supabaseAnonKey = 'sb_publishable_z7CnMXxLezgVpOPHaxDrYg_2xjK-YCJ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
